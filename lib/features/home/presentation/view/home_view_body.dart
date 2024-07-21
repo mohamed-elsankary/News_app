e6d19c68aa8ad595efea212fab6d18ea8a536b8c
Mohamed2002-G3 <mohamedelsankary14@gmail.com>
2024-07-21 15:12:22 +0300

    add box shadow to bottom navigation

--- lib/features/home/presentation/view/home_view_body.dart
+++ lib/features/home/presentation/view/home_view_body.dart
@@ -7,6 +7,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBarItemsApp(),
     );
   }
--- lib/features/home/presentation/view/widgets/bottom_navigation_bar_items.dart
+++ lib/features/home/presentation/view/widgets/bottom_navigation_bar_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavigationBarItemsApp extends StatelessWidget {
@@ -8,32 +9,40 @@ class BottomNavigationBarItemsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(15),
-      child: GNav(
-        activeColor: Colors.white,
-        color: Colors.grey,
-        tabBackgroundColor: Colors.blue,
-        padding: EdgeInsets.all(10),
-        gap: 8,
-        tabs: [
-          GButton(
-            icon: Icons.home,
-            text: 'Home',
-          ),
-          GButton(
-            icon: Icons.language,
-            text: 'Global',
-          ),
-          GButton(
-            icon: Icons.search,
-            text: 'Search',
-          ),
-          GButton(
-            icon: Icons.person_outline_rounded,
-            text: 'Profile',
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(0.1)),
         ],
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(15),
+        child: GNav(
+          activeColor: Colors.white,
+          color: Colors.grey,
+          tabBackgroundColor: Colors.blue,
+          padding: EdgeInsets.all(10),
+          gap: 8,
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.language,
+              text: 'Global',
+            ),
+            GButton(
+              icon: Icons.search,
+              text: 'Search',
+            ),
+            GButton(
+              icon: Icons.person_outline_rounded,
+              text: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
