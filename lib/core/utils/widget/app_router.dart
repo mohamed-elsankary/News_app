f4e5c186f121a902e09ea163af0da235247db92c
Mohamed2002-G3 <mohamedelsankary14@gmail.com>
2024-07-21 12:41:52 +0300

    add go router

---
+++ lib/core/utils/widget/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:go_router/go_router.dart';
+import 'package:news_app/features/home/home_view.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+
+  static final GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const HomeView();
+        },
+      ),
+      // GoRoute(
+      //   path: kSearchView,
+      //   builder: (context, state) {
+      //     return const SearchView();
+      //   },
+      // ),
+    ],
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/features/home/home_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NewsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
   }
 }
